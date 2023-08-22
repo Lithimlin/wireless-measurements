84c71d490ab6d551555d14a92e4c366faa08fae1
Julian Euler <jeuler@seemoo.tu-darmstadt.de>
2023-08-22 15:21:10 +0900

    refactor with pydantic and add points output - now uses pydantic models - can ouput all points in a file as well - functionality to filter out points

--- coords.py
+++ coords.py
@@ -1,12 +1,14 @@
 import json
 import logging
-from typing import Any, Optional
+from itertools import groupby
+from typing import Any, Callable, Optional
 
 import matplotlib.pyplot as plt  # type: ignore[import]
 import numpy as np
 import pandas as pd  # type: ignore[import]
 import seaborn as sns  # type: ignore[import]
 from mpl_toolkits.mplot3d.axes3d import Axes3D  # type: ignore[import]
+from pydantic import BaseModel, Field, model_serializer
 from scipy.spatial import distance  # type: ignore[import]
 
 logger = logging.getLogger(__name__)
@@ -23,16 +25,67 @@ _handler.setFormatter(_formatter)
 logger.addHandler(_handler)
 
 
-def generate_circular_3d_points(
-    radius: float, n_points: int, height: float
-) -> np.ndarray:
+class MissionConfig(BaseModel):
+    radius: float = Field(..., ge=0.0)
+    numStops: int = Field(..., ge=0)
+    altitude: float = Field(..., ge=0.0)
+
+
+class MissionPoint(BaseModel):
+    dlat: float
+    dlon: float
+    altitude: float = Field(..., ge=0.0)
+
+    def to_ndarray(self) -> np.ndarray:
+        return np.array([self.dlon, self.dlat, self.altitude])
+
+
+class MissionConfigs(BaseModel):
+    missions: list[MissionConfig]
+
+    def append(self, mission: MissionConfig) -> None:
+        self.missions.append(mission)
+
+    def extend(self, other: "MissionConfigs") -> None:
+        self.missions.extend(other.missions)
+
+
+class MissionPoints(BaseModel):
+    points: list[MissionPoint]
+
+    def to_ndarray(self) -> np.ndarray:
+        return np.array([point.to_ndarray() for point in self.points])
+
+    def append(self, point: MissionPoint) -> None:
+        self.points.append(point)
+
+    def extend(self, other: "MissionPoints") -> None:
+        self.points.extend(other.points)
+
+    def keep(self, predicate: Callable[[MissionPoint], bool]) -> "MissionPoints":
+        return MissionPoints(
+            points=[point for point in self.points if predicate(point)]
+        )
+
+    def drop(self, predicate: Callable[[MissionPoint], bool]) -> "MissionPoints":
+        return MissionPoints(
+            points=[point for point in self.points if not predicate(point)]
+        )
+
+
+def generate_circular_3d_points(config: MissionConfig) -> MissionPoints:
     logger.debug(
-        f"Generating {n_points} point{'s' if n_points > 1 else ''} at radius {radius} and height {height}"
+        f"Generating {config.numStops} point{'s' if config.numStops > 1 else ''} at radius {config.radius} and height {config.altitude}"
     )
-    theta = np.linspace(0, 2 * np.pi, n_points, endpoint=False)
-    x = radius * np.cos(theta)
-    y = radius * np.sin(theta)
-    return np.vstack((x, y, np.ones(n_points) * height)).T
+    theta = np.linspace(0, 2 * np.pi, config.numStops, endpoint=False)
+    x = config.radius * np.cos(theta)
+    y = config.radius * np.sin(theta)
+    return MissionPoints(
+        points=[
+            MissionPoint(dlon=dlon, dlat=dlat, altitude=config.altitude)
+            for dlon, dlat in zip(x, y)
+        ]
+    )
 
 
 def calc_n_points_at_radius(radius: float, arc_segment_length: float) -> int:
@@ -63,15 +116,13 @@ def calc_n_heights(
     return heights
 
 
-def plot_and_get_spaced_points(
-    ax: Axes3D,
+def get_point_missions(
     radius: float,
     r_spacing: float,
     min_height: float,
     v_spacing: Optional[float] = None,
     max_height: Optional[float] = None,
-    **kwargs,
-) -> tuple[np.ndarray, np.ndarray]:
+) -> MissionConfigs:
     if max_height is None:
         max_height = radius
 
@@ -81,8 +132,7 @@ def plot_and_get_spaced_points(
     heights = calc_n_heights(radius, v_spacing, min_height, max_height)
     logger.debug(f"Heights: {heights}")
 
-    all_points = np.empty(shape=(0, 3))
-    settings = np.empty(shape=(0, 3))
+    settings = MissionConfigs(missions=[])
 
     for height in heights:
         max_rad = np.sqrt(np.square(radius) - np.square(height))
@@ -93,24 +143,25 @@ def plot_and_get_spaced_points(
             n_points = int(calc_n_points_at_radius(r, r_spacing))
             n_points = max(n_points, 1)
 
-            points = generate_circular_3d_points(r, n_points, height)
-            ax.scatter(*points.T, **kwargs)
+            config = MissionConfig(radius=r, numStops=n_points, altitude=height)
+            settings.append(config)
 
-            all_points = np.concatenate((all_points, points), axis=0)
-            settings = np.append(settings, [[height, r, n_points]], axis=0)
-
-    return all_points, settings
+    return settings
 
 
-def plot_and_get_spaced_paths(
-    ax: Axes3D,
+def plot_point_missions(ax: Axes3D, settings: MissionConfigs, **kwargs) -> None:
+    for config in settings.missions:
+        points = generate_circular_3d_points(config)
+        ax.scatter(*points.to_ndarray().T, **kwargs)
+
+
+def get_path_missions(
     radius: float,
     r_spacing: float,
     min_height: float,
     v_spacing: Optional[float] = None,
     max_height: Optional[float] = None,
-    **kwargs,
-) -> np.ndarray:
+) -> MissionConfigs:
     if max_height is None:
         max_height = radius
 
@@ -120,37 +171,62 @@ def plot_and_get_spaced_paths(
     heights = calc_n_heights(radius, v_spacing, min_height, max_height)
     logger.debug(f"Heights: {heights}")
 
-    settings = np.empty(shape=(0, 2))
+    settings = MissionConfigs(missions=[])
 
     for height in heights:
-        if radius == height:
-            ax.scatter(0, 0, height, **kwargs)
-            continue
-
         max_rad = np.sqrt(np.square(radius) - np.square(height))
         radii = np.arange(max_rad, 0, -r_spacing)
         for r in radii[::-1]:
-            theta = np.linspace(0, 2 * np.pi, 50)
-            x = r * np.cos(theta)
-            y = r * np.sin(theta)
-            ax.plot(x, y, height, **kwargs)
-
-            settings = np.append(settings, [[height, r]], axis=0)
+            settings.append(MissionConfig(radius=r, numStops=0, altitude=height))
 
     return settings
 
 
-def save_settings(settings: np.ndarray, filename: str) -> None:
-    if settings.ndim == 2:
-        settings = np.c_[settings, np.zeros(shape=(settings.shape[0]))]
-    missions: list[dict] = [
-        dict(altitude=setting[0], radius=setting[1], numStops=int(setting[2]))
-        for setting in settings
-    ]
-    data = {"missions": missions}
+def plot_path_missions(ax: Axes3D, settings: MissionConfigs, **kwargs) -> None:
+    theta = np.linspace(0, 2 * np.pi, 50)
 
+    for config in settings.missions:
+        if config.radius == 0:
+            ax.scatter(0, 0, config.altitude, **kwargs)
+            continue
+
+        x = config.radius * np.cos(theta)
+        y = config.radius * np.sin(theta)
+        ax.plot(x, y, config.altitude, **kwargs)
+
+
+def order_points(settings: MissionConfigs) -> MissionPoints:
+    missions = settings.missions.copy()
+    all_points: MissionPoints = MissionPoints(points=[])
+    all_points.extend(generate_circular_3d_points(missions.pop(0)))
+
+    for i, (_, alt_missions) in enumerate(
+        groupby(missions, lambda mission: mission.altitude)
+    ):
+        for mission in alt_missions if i % 2 == 0 else list(alt_missions)[::-1]:
+            points = generate_circular_3d_points(mission)
+            distances = distance.cdist(
+                [all_points.points[-1].to_ndarray()], points.to_ndarray()
+            )
+            min_index = np.argmin(distances)
+            all_points.points.extend(points.points[min_index:])
+            all_points.points.extend(points.points[:min_index])
+
+    return all_points
+
+
+def plot_points_line(ax: Axes3D, points: MissionPoints, **kwargs) -> None:
+    ax.plot(*points.to_ndarray().T, **kwargs)
+
+
+def save_settings(settings: MissionConfigs, filename: str) -> None:
     with open(filename, "w") as f:
-        json.dump(data, f, indent=2)
+        json.dump(settings.model_dump(mode="json"), f, indent=2)
+
+
+def save_points(points: MissionPoints, filename: str) -> None:
+    with open(filename, "w") as f:
+        json.dump(points.model_dump(mode="json"), f, indent=2)
 
 
 def main():
@@ -171,33 +247,46 @@ def main():
     )
 
     logger.info("Generating points...")
-    all_points, settings = plot_and_get_spaced_points(
-        ax,
+    settings = get_point_missions(
         radius=radius,
         r_spacing=r_spacing,
         v_spacing=v_spacing,
         min_height=min_height,
         max_height=max_height,
     )
-    logger.info(f"Generated {all_points.shape[0]} points")
-    distances = distance.pdist(all_points)
+    plot_point_missions(ax, settings)
+
+    all_points = MissionPoints(points=[])
+    [
+        all_points.extend(generate_circular_3d_points(mission))
+        for mission in settings.missions
+    ]
+    logger.info(f"Generated {len(all_points.points)} points")
+    distances = distance.pdist(all_points.to_ndarray())
     logger.info(f"Min distance: {np.min(distances)}")
     logger.debug(f"Mean distance: {np.mean(distances)}")
 
     save_settings(settings, "missions.jsonc")
 
-    logger.info("Generating paths...")
-    settings = plot_and_get_spaced_paths(
-        ax,
-        radius=radius,
-        r_spacing=r_spacing,
-        v_spacing=v_spacing,
-        min_height=min_height,
-        max_height=max_height,
+    ordered_points = order_points(settings)
+    filtered_points = ordered_points.drop(
+        lambda point: point.altitude == 3.0 and point.dlon > 100.0
     )
-    logger.info(f"Generated {settings.shape[0]} paths")
+    plot_points_line(ax, filtered_points)
+    save_points(ordered_points, "points.jsonc")
 
-    save_settings(settings, "paths.jsonc")
+    # logger.info("Generating paths...")
+    # settings = get_path_missions(
+    #     radius=radius,
+    #     r_spacing=r_spacing,
+    #     v_spacing=v_spacing,
+    #     min_height=min_height,
+    #     max_height=max_height,
+    # )
+    # plot_path_missions(ax, settings)
+    # logger.info(f"Generated {len(settings.missions)} paths")
+
+    # save_settings(settings, "paths.jsonc")
 
     plt.tight_layout()
     plt.show()
